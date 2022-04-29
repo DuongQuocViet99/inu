9d1ef47475912c8d81cd49dbd7259fc965092536
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-04-29 22:59:18 +0700

    create: custom theme

---
+++ src/components/theme.js
@@ -0,0 +1,15 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    adobe: {
+      1: '#E6E6E6',
+      2: '#D9D9D9',
+      3: '#BFBFBF',
+      4: '#999999',
+      5: '#595959'
+    }
+  }
+})
+
+export default theme;
