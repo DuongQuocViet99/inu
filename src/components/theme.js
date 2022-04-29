5d64d5a4529a95189623bdab6a2caeab97996f05
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-04-30 01:00:12 +0700

    add new color

--- src/components/theme.js
+++ src/components/theme.js
@@ -3,13 +3,16 @@ import { extendTheme } from "@chakra-ui/react";
 const theme = extendTheme({
   colors: {
     adobe: {
+      0: '#F2F2F2',
       1: '#E6E6E6',
       2: '#D9D9D9',
       3: '#BFBFBF',
-      4: '#999999',
-      5: '#595959'
+      4: '#A6A6A6',
+      5: '#737373',
+      6: '#999999',
+      7: '#595959'
     }
   }
-})
+});
 
 export default theme;
