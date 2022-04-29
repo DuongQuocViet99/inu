661373c8b14bf0f8ace6a4ebb52d62515f507b55
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-04-29 23:24:08 +0700

    use Routes & Route( Layout )

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router-dom';
 
 import theme from 'components/theme';
+import Layout from 'pages/layout';
 import './App.css';
 
 export default function App() {
   return (
     <>
       <ChakraProvider theme={ theme }>
-        
+        <Routes>
+          <Route path='/' element={ <Layout /> }>
+            
+          </Route>
+        </Routes>
       </ChakraProvider>
     </>
   );
