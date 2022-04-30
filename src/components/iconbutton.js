383e8b49ca7015dd63d706717c7caf19552494e7
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-04-30 14:32:45 +0700

    remove IconButtonOrigin & add new IconButtonNew, IconButtonShadow

--- src/components/iconbutton.js
+++ src/components/iconbutton.js
@@ -1,31 +1,40 @@
 import { IconButton } from "@chakra-ui/react";
 import { CgMenuMotion } from "react-icons/cg";
+import { HiPlus } from "react-icons/hi";
 
-export default function IconButtonOrigin( props ) {
-  return (
-    <>
-      <IconButton
-        cursor='default'
-        _focus={{ outline: 'none' }}
-        { ...props }
-      />
-    </>
-  );
-}
+export const IconButtonDrawer = props => (
+  <IconButton
+    size='sm'
+    bg='white'
+    color='adobe.6'
+    fontSize='24px'
+    cursor='default'
+    icon={ <CgMenuMotion /> }
+    _hover={{ bg: 'adobe.0' }}
+    _focus={{ outline: 'none' }}
+    _active={{}}
+    { ...props }
+  />
+);
 
-export function IconButtonDrawer( props ) {
-  return (
-    <>
-      <IconButtonOrigin
-        size='sm'
-        bg='white'
-        color='adobe.6'
-        fontSize='24px'
-        icon={ <CgMenuMotion /> }
-        _hover={{ bg: 'adobe.0' }}
-        _active={{}}
-        { ...props }
-      />
-    </>
-  );
-}
+export const IconButtonShadow = props => (
+  <IconButton
+    bg='white'
+    boxShadow='md'
+    color='adobe.6'
+    fontSize='18px'
+    cursor='default'
+    _hover={{ boxShadow: 'base' }}
+    _focus={{ outline: 'none' }}
+    _active={{}}
+    { ...props }
+  />
+);
+
+export const IconButtonNew = props => (
+  <IconButtonShadow
+    size='sm'
+    icon={ <HiPlus /> }
+    { ...props }
+  />
+);
