0b8ed08f53aa5ab62fd93c8d4d43935bafc6ca1f
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-05-01 01:28:16 +0700

    new: IconButtonPopover & IconButtonPopoverItem

--- src/components/iconbutton.js
+++ src/components/iconbutton.js
@@ -1,3 +1,4 @@
+import { FiChevronRight } from "react-icons/fi";
 import { IconButton } from "@chakra-ui/react";
 import { CgMenuMotion } from "react-icons/cg";
 import { HiPlus } from "react-icons/hi";
@@ -10,9 +11,9 @@ export const IconButtonDrawer = props => (
     fontSize='24px'
     cursor='default'
     icon={ <CgMenuMotion /> }
-    _hover={{ bg: 'adobe.0' }}
-    _focus={{ outline: 'none' }}
+    _focus={{}}
     _active={{}}
+    _hover={{ bg: 'adobe.0' }}
     { ...props }
   />
 );
@@ -24,9 +25,9 @@ export const IconButtonShadow = props => (
     color='adobe.6'
     fontSize='18px'
     cursor='default'
-    _hover={{ boxShadow: 'base' }}
-    _focus={{ outline: 'none' }}
+    _focus={{}}
     _active={{}}
+    _hover={{ boxShadow: 'base' }}
     { ...props }
   />
 );
@@ -35,6 +36,35 @@ export const IconButtonNew = props => (
   <IconButtonShadow
     size='sm'
     icon={ <HiPlus /> }
+    { ...props }
+  />
+);
+
+export const IconButtonPopover = props => (
+  <IconButton
+    size='sm'
+    bg='none'
+    fontSize='xl'
+    color='adobe.6'
+    cursor='default'
+    icon={ <FiChevronRight /> }
+    _focus={{}}
+    _hover={{}}
+    _active={{}}
+    { ...props }
+  />
+);
+
+export const IconButtonPopoverItem = props => (
+  <IconButton
+    size='xs'
+    bg='none'
+    rounded='sm'
+    color='adobe.3'
+    cursor='default'
+    _focus={{}}
+    _hover={{ bg: 'blackAlpha.500', color: 'white' }}
+    _active={{}}
     { ...props }
   />
 );
