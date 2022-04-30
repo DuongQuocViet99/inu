e08b880df60a77afe0024ba7d373055196e78b65
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-05-01 06:35:32 +0700

    create: project item

---
+++ src/components/item.js
@@ -0,0 +1,29 @@
+import { Flex, GridItem, LinkBox, LinkOverlay, Spacer } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+import PopoverControls from "components/popover.controls";
+import TextS from "components/text";
+
+export default function Item() {
+  return (
+    <>
+      <LinkBox>
+        <GridItem
+          p={ 1 }
+          pr={ 0 }
+          pl={ 3 }
+          as={ Flex }
+          boxShadow='md'
+          alignItems='center'
+          _hover={{ boxShadow: 'base' }}
+        >
+          <LinkOverlay as={ Link } to='/inu'>
+            <TextS>Inu</TextS>
+          </LinkOverlay>
+          <Spacer />
+          <PopoverControls />
+        </GridItem>
+      </LinkBox>
+    </>
+  );
+}
