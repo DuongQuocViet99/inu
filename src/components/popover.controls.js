bbdf7b46d1a11485da8ea59a6ec0a063a2624607
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-05-01 06:32:19 +0700

    create: project controls

---
+++ src/components/popover.controls.js
@@ -0,0 +1,53 @@
+import { Box, Popover, PopoverTrigger, PopoverContent, PopoverBody, HStack } from '@chakra-ui/react';
+import { FiEdit2, FiX } from 'react-icons/fi';
+import { CgTrash } from 'react-icons/cg';
+
+import { IconButtonPopover, IconButtonPopoverItem } from 'components/iconbutton';
+import { DividerVertical } from 'components/divider';
+
+export default function PopoverControls() {
+  return (
+    <>
+      <Popover placement='bottom-end'>
+        {({ isOpen, onClose }) => (
+          <>
+            <PopoverTrigger>
+              <Box>
+                <IconButtonPopover
+                  transform={ isOpen ? 'rotate(90deg)': 'rotate(0deg)' }
+                />
+              </Box>
+            </PopoverTrigger>
+            <PopoverContent
+              w='auto'
+              bg='adobe.7'
+              rounded='sm'
+              border='none'
+              _focus={{}}
+            >
+              <PopoverBody p={ 1 }>
+                <HStack>
+                  <IconButtonPopoverItem
+                    fontSize='17px'
+                    icon={ <FiEdit2 /> }
+                  />
+                  <DividerVertical />
+                  <IconButtonPopoverItem
+                    fontSize='19px'
+                    icon={ <CgTrash /> }
+                  />
+                  <DividerVertical />
+                  <IconButtonPopoverItem
+                    fontSize='18px'
+                    onClick={ onClose }
+                    icon={ <FiX /> }
+                  />
+                </HStack>
+              </PopoverBody>
+            </PopoverContent>
+          </>
+        )}        
+      </Popover>
+    </>
+  );
+}
