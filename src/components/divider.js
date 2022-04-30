14896639e65a6fd546bc2edb1bb7481d8a27c906
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-04-30 22:05:19 +0700

    new: DividerVertical

--- src/components/divider.js
+++ src/components/divider.js
@@ -1,8 +1,17 @@
 import { Divider } from "@chakra-ui/react";
 
 export const DividerHorizontal = props => (
-  <Divider 
-    borderColor='adobe.3' 
+  <Divider
+    borderColor='adobe.3'
+    { ...props }
+  />
+);
+
+export const DividerVertical = props => (
+  <Divider
+    h='12px'
+    orientation='vertical'
+    borderColor='whiteAlpha.400'
     { ...props }
   />
 );
