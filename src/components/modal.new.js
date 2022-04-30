8b16305f1c7c450da87249b40d938c0cb2e682cb
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-04-30 16:53:06 +0700

    create: new project

---
+++ src/components/modal.new.js
@@ -0,0 +1,45 @@
+import * as C from '@chakra-ui/react';
+
+import { IconButtonNew } from "components/iconbutton";
+import { ButtonShadow } from 'components/button';
+import { InputOutline } from 'components/input';
+
+export default function New() {
+  const { isOpen, onOpen, onClose } = C.useDisclosure();
+
+  return (
+    <>
+      <IconButtonNew onClick={ onOpen } />
+
+      <C.Modal
+        size='xl'
+        isCentered
+        isOpen={ isOpen } 
+        onClose={ onClose }
+        motionPreset='slideInBottom'
+      >
+        <C.ModalOverlay bg='rgba(89, 89, 89, 0.4)' />
+        <C.ModalContent rounded='sm'>
+          <C.ModalCloseButton
+            _focus={{}}
+            color='adobe.5'
+          />
+          <C.ModalHeader
+            color='adobe.7'
+            fontFamily='Quicksand'
+          >
+            New project
+          </C.ModalHeader>
+          <C.ModalBody>
+            <InputOutline placeholder='Project name' />
+          </C.ModalBody>
+          <C.ModalFooter>
+            <ButtonShadow onClick={ onClose }>
+              Create
+            </ButtonShadow>
+          </C.ModalFooter>
+        </C.ModalContent>
+      </C.Modal>
+    </>
+  );
+}
