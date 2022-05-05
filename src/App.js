2289ba8967b4201c059b336f1807fffbfbc1e242
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-05-05 10:42:15 +0700

    create routes: /todo /mess & /team

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Route, Routes } from 'react-router-dom';
 import theme from 'components/theme';
 import Layout from 'pages/layout';
 import Home from 'pages/home';
+import Todo from 'pages/todo';
+import Mess from 'pages/mess';
+import Team from 'pages/team';
 import './App.css';
 
 export default function App() {
@@ -13,6 +16,9 @@ export default function App() {
         <Routes>
           <Route path='/' element={ <Layout /> }>
             <Route index element={ <Home /> } />
+            <Route path=':projectID/todo' element={ <Todo /> }/>
+            <Route path=':projectID/mess' element={ <Mess /> }/>
+            <Route path=':projectID/team' element={ <Team /> }/>
           </Route>
         </Routes>
       </ChakraProvider>
