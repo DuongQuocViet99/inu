203b2f5f7545e4b9356d2a291ad98237ef6a8965
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-05-22 20:57:14 +0700

    create: AvatarUnstyled & AvatarActivate

---
+++ src/components/avatar.js
@@ -0,0 +1,24 @@
+import { Avatar } from "@chakra-ui/react";
+
+export const AvatarUnstyled = ({ name }) => (
+  <Avatar
+    name={ name }
+    bg='none'
+    size='none'
+    color='adobe.6'
+    fontWeight='700' 
+  />
+)
+
+export const AvatarActivate = ({ name }) => (
+  <Avatar
+    name={ name }
+    size='sm'
+    bg='adobe.5'
+    color='white'
+    cursor='pointer'
+    fontWeight='700'
+    fontFamily='Quicksand'
+    _hover={{ bg: 'adobe.7' }}
+  />
+)
