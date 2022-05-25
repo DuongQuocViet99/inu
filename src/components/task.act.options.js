47c91e2ab54fb7629c5e8ab101fe0718ebc58301
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-05-25 11:35:43 +0700

    create: task options

---
+++ src/components/task.act.options.js
@@ -0,0 +1,49 @@
+import { Box, Stack } from "@chakra-ui/react";
+
+import FormControlS from "components/formcontrol";
+import { InputOutline } from "components/input";
+import UserOption from "components/user.option";
+import SwitchS from "components/Switch";
+import { ButtonShadow } from "./button";
+
+export default function TaskOptions() {
+  return(
+    <>
+      <Stack>
+        <FormControlS 
+          name='taskname' 
+          formLabel='task name'
+        >
+          <InputOutline id='taskname' placeholder="Task name" />
+        </FormControlS>
+
+        <FormControlS 
+          name='duedate'
+          formLabel='due date'
+        >
+          <InputOutline id='duedate' placeholder="Due date" />
+        </FormControlS>
+
+        <FormControlS 
+          name='priority'
+          formLabel='priority'
+          formHelperText='Priority mission'
+        >
+          <SwitchS />
+        </FormControlS>
+
+        <FormControlS 
+          name='assigne'
+          formLabel='assigne'
+        >
+          <UserOption />
+        </FormControlS>
+        <Box textAlign='right'>
+          <ButtonShadow w='min'>
+            Save
+          </ButtonShadow>
+        </Box>
+      </Stack>
+    </>
+  );
+}
