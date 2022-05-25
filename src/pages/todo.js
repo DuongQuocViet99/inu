ccd8e441fa78edbb6ed7af76821251fa56769964
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-05-26 04:07:11 +0700

    chore: update

--- src/pages/todo.js
+++ src/pages/todo.js
@@ -1,7 +1,14 @@
+import { Box } from "@chakra-ui/react";
+import Process from "components/process";
+
 export default function Todo() {
   return (
     <>
-      
+      <Box p={ 6 }>
+        <Process color='coolors.3'>review</Process>
+        <Process color='#89b0ae'>planning</Process>
+        <Process color='coolors.5'>open</Process>
+      </Box>
     </>
   );
 }
