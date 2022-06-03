b3ca55c929fa8713de3b7271111d6d3bbfd6b851
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-06-03 21:47:47 +0700

    create: InputMess

--- src/components/input.js
+++ src/components/input.js
@@ -14,3 +14,20 @@ export const InputOutline = props => (
     { ...props }
   />
 );
+
+export const InputMess = props => (
+  <Input
+    bg='white'
+    rounded='sm'
+    border='none'
+    boxShadow='md'
+    color='adobe.7'
+    fontWeight='600'
+    fontFamily='Quicksand'
+    placeholder="Type message"
+    _hover={{ bg: 'adobe.1' }}
+    _placeholder={{ color: 'adobe.6' }}
+    _focus={{ bg: 'white', border: '2px', borderColor: 'adobe.3' }}
+    { ...props }
+  />
+);
