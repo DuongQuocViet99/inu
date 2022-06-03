e3be963488397f56f285d2f6457b8350ad4e4e28
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-06-03 21:49:07 +0700

    UI: update

--- src/pages/mess.js
+++ src/pages/mess.js
@@ -1,7 +1,46 @@
+import { Box, HStack, useBoolean } from "@chakra-ui/react";
+import { FiPaperclip, FiSend } from "react-icons/fi";
+
+import { IconButtonShadow } from "components/iconbutton";
+import { InputMess } from "components/input";
+import { useEffect, useRef } from "react";
+import MessIb from "components/mess.ib";
+
 export default function Mess() {
+  const [ pin, setPin ] = useBoolean();
+  const boxRef = useRef( null );
+  useEffect(() => {
+    boxRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end'
+    })
+  }, [ boxRef ]);
+
   return (
     <>
-      
+      <Box
+        p={ 4 }
+        pb={ 24 }
+        ref={ boxRef }
+      >
+        <MessIb me>hôm nay trời đẹp</MessIb>
+        <MessIb>dung vay</MessIb>
+      </Box>
+      <HStack
+        p={ 4 }
+        w='full'
+        bg='white'
+        bottom={ 0 }
+        position='fixed'
+      >
+        <IconButtonShadow
+          icon={ <FiPaperclip /> } 
+          onClick={ setPin.toggle }
+          color={ pin ? 'adobe.7' : 'adobe.6' }
+        />
+        <InputMess />
+        <IconButtonShadow icon={ <FiSend /> } />
+      </HStack>
     </>
   );
 }
