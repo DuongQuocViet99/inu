750dc12df7765b0c0f8a8aff3bce5da90a9b4680
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-06-05 22:21:42 +0700

    custom checkbox

--- src/components/theme.js
+++ src/components/theme.js
@@ -10,7 +10,9 @@ const theme = extendTheme({
       4: '#A6A6A6',
       5: '#737373',
       6: '#999999',
-      7: '#595959'
+      7: '#595959',
+      8: '#BFB9B8',
+      9: '#A6A1A1'
     },
     coolors: {
       1: '#FFCDB2',
@@ -18,6 +20,30 @@ const theme = extendTheme({
       3: '#E5989B',
       4: '#B5838D',
       5: '#6D6875'
+    }
+  },
+  components: {
+    Checkbox: {
+      baseStyle: {
+        control: {
+          borderColor: '#BFB9B8',
+          _checked: {
+            bg: '#BFB9B8',
+            borderColor: '#BFB9B8',
+            _hover: {
+              bg: '#BFB9B8',
+              borderColor: '#BFB9B8',
+            }
+          },
+          _indeterminate: {
+            bg: '#BFB9B8',
+            borderColor: '#BFB9B8'
+          },
+          _focus: {
+            boxShadow: 'none'
+          }
+        }
+      }
     }
   }
 });
