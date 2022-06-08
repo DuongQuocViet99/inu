28ebcaf8df9d91780d0ff8fcd048d1a277b8d75d
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-06-08 18:49:36 +0700

    UI: update

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,12 +1,51 @@
-import { Outlet } from "react-router-dom";
+import { Grid, GridItem, VStack } from "@chakra-ui/react";
+import { FiBox, FiSettings } from "react-icons/fi";
+import { Link, Outlet } from "react-router-dom";
 
+import { IconButtonSidebar } from "components/iconbutton";
+import DrawerS from "components/drawer";
 import Header from "components/header";
+import Map from "render/map";
+
+const sidebarItem = [
+  { icon: FiBox, url: '/' },
+  { icon: FiSettings, url: '/settings' }
+];
 
 export default function Layout() {
   return (
     <>
-      <Header />
-      <Outlet />
+      <Grid templateColumns='repeat(20, 1fr)'>
+        <GridItem colSpan={ 1 }>
+          <VStack
+            p={ 5 }
+            top={ 0 }
+            spacing={ 12 }
+            position='sticky'
+          >
+            <DrawerS />
+            <VStack spacing={ 5 }>
+              <Map 
+                data={ sidebarItem }
+                render={( i, k ) => 
+                  <IconButtonSidebar
+                    key={ k }
+                    as={ Link }
+                    to={ i.url }
+                    icon={ <i.icon /> }
+                    bg={ i.url !== '/settings' ? '#F2EDE4' : 'none' }
+                    color={ i.url !== '/settings' ? 'adobe.8' : '#D9D9D9' }
+                  />
+                }
+              />
+            </VStack>
+          </VStack>
+        </GridItem>
+        <GridItem colSpan={ 19 }>
+          <Header />
+          <Outlet />
+        </GridItem>
+      </Grid>
     </>
   );
 }
